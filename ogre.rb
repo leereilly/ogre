2272f2f3bc7a59c8c93d9ac72232ca13f57fe9da
Lee Reilly <lee@leereilly.net>
2014-04-18 20:17:36 -0700

    Test version

--- ogre.rb
+++ ogre.rb
@@ -3,10 +3,10 @@ require 'sinatra'
 require 'oily_png'
 
 get '/' do
-  png = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color::TRANSPARENT)
-  png[1,1] = ChunkyPNG::Color.rgba(10, 20, 30, 128)
-  png[2,1] = ChunkyPNG::Color('black @ 0.5')
-  png.save('test.png', :interlace => true)
+  background = ChunkyPNG::Image.from_file('filename.png')
+  blacktocat = ChunkyPNG::Image.from_file('blacktocat.png')
+  background.compose!(blacktocat, 10, 10)
+  background.save('leereilly.png', :fast_rgba)
 
   erb :index
 end
